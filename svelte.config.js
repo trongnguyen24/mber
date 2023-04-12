d775997c21f7e67f9c3e6258f014235034b69838
trongnguyen24 <trongnguyen24@gmail.com>
2023-04-12 10:52:18 +0700

    vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,18 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+// import adapter from '@sveltejs/adapter-auto';
+// import { vitePreprocess } from '@sveltejs/kit/vite';
+
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	kit: {
+// 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+// 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+// 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+// 		adapter: adapter()
+// 	},
+// 	preprocess: vitePreprocess()
+// };
+
+// export default config;
 
 // import adapter from '@sveltejs/adapter-node';
 // export default {
@@ -9,15 +22,14 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 // 	preprocess: vitePreprocess()
 // };
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
+import adapter from '@sveltejs/adapter-vercel';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+
+export default {
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			// see the 'Deployment configuration' section below
+		})
 	},
 	preprocess: vitePreprocess()
 };
-
-export default config;
